453f7ef1f272f1e3a6f2ff6049eb4038385a1ae3
wjh56 <569901307@qq.com>
2021-11-28 16:55:22 +0800

    3-7封装请求参数和返回参数

--- src/main/java/com/wjh/wiki/service/EbookService.java
+++ src/main/java/com/wjh/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.wjh.wiki.service;
 
 import com.wjh.wiki.domain.Ebook;
+import com.wjh.wiki.domain.EbookExample;
 import com.wjh.wiki.mapper.EbookMapper;
+import com.wjh.wiki.req.EbookReq;
+import com.wjh.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,8 +18,20 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(){
+    public List<EbookResp> list(EbookReq req){
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        return ebookMapper.selectByExample(null);
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);
+           // ebookResp.setId(123L);   //测试用
+            respList.add(ebookResp);
+        }
+
+        return respList;
     }
 }
