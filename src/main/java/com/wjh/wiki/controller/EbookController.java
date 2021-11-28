35454005f6ad33eec74317c3c5c3fbeae6ef8df5
wjh56 <569901307@qq.com>
2021-11-28 15:02:11 +0800

    3-6新建一个CommonResp类来规范返回值，后端所有返回值都是用这个

--- src/main/java/com/wjh/wiki/controller/EbookController.java
+++ src/main/java/com/wjh/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.wjh.wiki.controller;
 
 import com.wjh.wiki.domain.Ebook;
+import com.wjh.wiki.resp.CommonResp;
 import com.wjh.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +18,11 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list(){
-        return ebookService.list();
+    public CommonResp list(){
+        CommonResp<List<Ebook>> resp=new CommonResp<>();
+        List<Ebook> list=ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 
     
