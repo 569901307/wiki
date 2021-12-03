86138817952ff165ee5ada89f86b62de44d9e3db
wjh56 <569901307@qq.com>
2021-12-03 17:21:01 +0800

    5-4列表查询接口支持动态sql,改图标成为方形

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -38,7 +38,7 @@
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
 
-      <a-list item-layout="vertical" size="large":grid="{ gutter:20, column: 3 }"  :data-source="ebooks">
+      <a-list item-layout="vertical" size="large" :grid="{ gutter:20, column: 3 }"  :data-source="ebooks">
 
         <template #renderItem="{ item }">
           <a-list-item key="item.name">
@@ -104,7 +104,7 @@ export default defineComponent({
 
      onMounted(()=>{
          console.log("onMounted");
-         axios.get("http://localhost:8880/ebook/list?name=Spring").then((response)=>{
+         axios.get("http://localhost:8880/ebook/list").then((response)=>{
              const data=response.data;
              ebooks.value=data.content;
              ebooks1.books=data.content;
@@ -121,3 +121,13 @@ export default defineComponent({
   }
 });
 </script>
+
+<style scoped>
+    .ant-avatar {
+        width: 50px;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 8%;
+        margin: 5px 0;
+    }
+</style>
