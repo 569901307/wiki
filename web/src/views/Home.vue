4576854bd1a0b44d086c0825cc0c0d67ec8c85a7
wjh56 <569901307@qq.com>
2021-12-04 08:59:07 +0800

    5-6配置axios拦截器来打印请求参数和返回参数

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -86,7 +86,6 @@ for (let i = 0; i < 23; i++) {
 export default defineComponent({
   name: 'Home',
   setup(){
-     console.log("setup");
      const ebooks=ref();
      const ebooks1=reactive({books:[]});
 
@@ -103,12 +102,11 @@ export default defineComponent({
       ];
 
      onMounted(()=>{
-         console.log("onMounted2222");
          axios.get("/ebook/list").then((response)=>{
              const data=response.data;
              ebooks.value=data.content;
              ebooks1.books=data.content;
-             console.log(response);
+
          });
      });
       return{
