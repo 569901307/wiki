98a5f823593e275f90c05bb94f6d41e2e85ef5eb
wjh56 <569901307@qq.com>
2021-12-05 18:30:31 +0800

    6-4使用pagehelper实现后端分页

--- src/main/java/com/wjh/wiki/service/EbookService.java
+++ src/main/java/com/wjh/wiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.wjh.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.wjh.wiki.domain.Ebook;
 import com.wjh.wiki.domain.EbookExample;
 import com.wjh.wiki.mapper.EbookMapper;
 import com.wjh.wiki.req.EbookReq;
 import com.wjh.wiki.resp.EbookResp;
 import com.wjh.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -15,16 +19,24 @@ import java.util.List;
 @Service
 public class EbookService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
     @Resource
     private EbookMapper ebookMapper;
 
     public List<EbookResp> list(EbookReq req){
+
+
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if (!ObjectUtils.isEmpty(req.getName())){
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        PageHelper.startPage(2,2);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo=new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
 
        /* List<EbookResp> respList = new ArrayList<>();
         for (Ebook ebook : ebookList) {
