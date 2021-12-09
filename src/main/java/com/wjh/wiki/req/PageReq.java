df531100339dfc0348bfc04404b021babec993cd
wjh56 <569901307@qq.com>
2021-12-09 15:43:04 +0800

    6-11集成Validation,为列表接口增加参数校验

--- src/main/java/com/wjh/wiki/req/PageReq.java
+++ src/main/java/com/wjh/wiki/req/PageReq.java
@@ -1,7 +1,14 @@
 package com.wjh.wiki.req;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
 public class PageReq {
+    @NotNull(message = "页码不能为空")
     private int page;
+
+    @NotNull(message = "每条页数不能为空")
+    @Max(value = 1000,message = "每条页数不能超过1000")
     private int size;
 
     public int getPage() {
