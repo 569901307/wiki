83b927f93cde9fcef9bda45a64a226f35d2ae29a
wjh56 <569901307@qq.com>
2021-12-12 13:29:09 +0800

    7-7首页显示分类菜单

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -1,37 +1,27 @@
-<template>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
   <a-layout>
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
               mode="inline"
               :style="{ height: '100%', borderRight: 0 }"
+              @click="handleClick"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
-            <span><user-outlined />subnav 11111</span>
+        <a-menu-item key="welcome">
+          <router-link :to="'/'">
+            <MailOutlined />
+            <span>欢迎</span>
+          </router-link>
+        </a-menu-item>
+
+        <a-sub-menu v-for="item in level1" :key="item.id">
+          <template v-slot:title>
+            <span><user-outlined />{{item.name}}</span>
           </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <MailOutlined /><span>{{child.name}}</span>
+          </a-menu-item>
         </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-            <span><laptop-outlined />subnav 2</span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-            <span><notification-outlined />subnav 3</span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
-        </a-sub-menu>
+
       </a-menu>
     </a-layout-sider>
     <a-layout-content
@@ -67,6 +57,8 @@
 <script lang="ts">
 import { defineComponent,onMounted,ref} from 'vue';
 import axios from 'axios';
+import { message } from 'ant-design-vue';
+import {Tool} from "../util/tools";
 
 /*const listData: Record<string, string>[] = [];
 
@@ -88,6 +80,30 @@ export default defineComponent({
   setup(){
      const ebooks=ref();
     // const ebooks1=reactive({books:[]});
+      const level1 =  ref();
+      let categorys: any;
+      /**
+       * 查询所有分类
+       **/
+      const handleQueryCategory = () => {
+          axios.get("/category/all").then((response) => {
+              const data = response.data;
+              if (data.success) {
+                  categorys = data.content;
+                  console.log("原始数组：", categorys);
+
+                  level1.value = [];
+                  level1.value = Tool.array2Tree(categorys, 0);
+                  console.log("树形结构：", level1.value);
+              } else {
+                  message.error(data.message);
+              }
+          });
+      };
+
+      const handleClick = () => {
+          console.log("menu click")
+      };
 
       const pagination = {
           onChange: (page: number) => {
@@ -102,6 +118,7 @@ export default defineComponent({
       ];
 
      onMounted(()=>{
+         handleQueryCategory();
          axios.get("/ebook/list",{
              params:{
                  page:1,
@@ -120,6 +137,8 @@ export default defineComponent({
           //listData,
           pagination,
           actions,
+          level1,
+          handleClick
       }
   }
 });
