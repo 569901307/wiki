717f1b242f155cee0f36e5cd4cfaf02b2a46cb42
wjh56 <569901307@qq.com>
2021-12-14 18:51:40 +0800

    8-3从分类文档复制出文档管理的前后端代码 遗留问题：编辑表单中，只能选择一级文档作为父文档

---
+++ src/main/java/com/wjh/wiki/controller/DocController.java
@@ -0,0 +1,56 @@
+package com.wjh.wiki.controller;
+
+import com.wjh.wiki.req.DocQueryReq;
+import com.wjh.wiki.req.DocSaveReq;
+import com.wjh.wiki.resp.DocQueryResp;
+import com.wjh.wiki.resp.CommonResp;
+import com.wjh.wiki.resp.PageResp;
+import com.wjh.wiki.service.DocService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/doc")
+public class DocController {
+    
+    @Resource
+    private DocService docService;
+
+    @GetMapping("/list")
+    public CommonResp list(@Valid DocQueryReq req){
+        CommonResp<PageResp<DocQueryResp>> resp=new CommonResp<>();
+        PageResp<DocQueryResp> list=docService.list(req);
+        resp.setContent(list);
+        return resp;
+    }
+
+    @GetMapping("/all")
+    public CommonResp all(){
+        CommonResp<List<DocQueryResp>> resp=new CommonResp<>();
+        List<DocQueryResp> list=docService.all();
+        resp.setContent(list);
+        return resp;
+    }
+
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody DocSaveReq req){
+        CommonResp resp=new CommonResp<>();
+        docService.save(req);
+        return resp;
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id){
+        CommonResp resp=new CommonResp<>();
+        docService.delete(id);
+        return resp;
+    }
+
+    
+
+
+}
---
+++ src/main/java/com/wjh/wiki/req/DocQueryReq.java
@@ -0,0 +1,10 @@
+package com.wjh.wiki.req;
+
+public class DocQueryReq extends PageReq{
+
+
+    @Override
+    public String toString() {
+        return "DocQueryReq{} " + super.toString();
+    }
+}
