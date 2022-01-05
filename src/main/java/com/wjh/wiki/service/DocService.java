2e0a7c4dc5c5b782710e6569c2a764c6d6f04f6d
wjh56 <569901307@qq.com>
2022-01-05 16:13:23 +0800

    10-11 针对点赞通知功能，使用异步化够了，没必要引入MQ

--- src/main/java/com/wjh/wiki/service/DocService.java
+++ src/main/java/com/wjh/wiki/service/DocService.java
@@ -18,7 +18,6 @@ import com.wjh.wiki.util.CopyUtil;
 import com.wjh.wiki.util.RedisUtil;
 import com.wjh.wiki.util.RequestContext;
 import com.wjh.wiki.util.SnowFlake;
-import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -42,8 +41,8 @@ public class DocService {
     @Resource
     public WsService wsService;
 
-    @Resource
-    private RocketMQTemplate rocketMQTemplate;
+//    @Resource
+//    private RocketMQTemplate rocketMQTemplate;
 
     @Resource
     private DocMapperCust docMapperCust;
@@ -166,8 +165,8 @@ public class DocService {
         // 推送消息
                Doc docDb = docMapper.selectByPrimaryKey(id);
                String logId = MDC.get("LOG_ID");
-              // wsService.sendInfo("【" + docDb.getName() + "】被点赞！", logId);
-              rocketMQTemplate.convertAndSend("VOTE_TOPIC", "【" + docDb.getName() + "】被点赞！");
+              wsService.sendInfo("【" + docDb.getName() + "】被点赞！", logId);
+             // rocketMQTemplate.convertAndSend("VOTE_TOPIC", "【" + docDb.getName() + "】被点赞！");
     }
 
 
