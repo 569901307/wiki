5a5d3fc566573ab57e3021485063170ac5551ba4
wjh56 <569901307@qq.com>
2022-01-11 14:20:06 +0800

    11-6 后端增加获取统计数值接口

---
+++ src/main/java/com/wjh/wiki/controller/EbookSnapshotController.java
@@ -0,0 +1,28 @@
+package com.wjh.wiki.controller;
+
+import com.wjh.wiki.resp.CommonResp;
+import com.wjh.wiki.resp.StatisticResp;
+import com.wjh.wiki.service.EbookSnapshotService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook-snapshot")
+public class EbookSnapshotController {
+
+    @Resource
+    private EbookSnapshotService ebookSnapshotService;
+
+    @GetMapping("/get-statistic")
+    public CommonResp getStatistic() {
+        List<StatisticResp> statisticResp = ebookSnapshotService.getStatistic();
+        CommonResp<List<StatisticResp>> commonResp = new CommonResp<>();
+        commonResp.setContent(statisticResp);
+        return commonResp;
+    }
+
+}
--- src/main/java/com/wjh/wiki/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/wjh/wiki/mapper/EbookSnapshotMapperCust.java
@@ -1,6 +1,12 @@
 package com.wjh.wiki.mapper;
 
+import com.wjh.wiki.resp.StatisticResp;
+
+import java.util.List;
+
 public interface EbookSnapshotMapperCust {
 
     public void genSnapshot();
+
+    List<StatisticResp> getStatistic();
 }
