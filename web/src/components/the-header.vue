27f76d4e1c4516a6c1b981cf3d226a139678fea5
wjh56 <569901307@qq.com>
2022-01-15 21:53:33 +0800

    11-9 修改LOGO展示

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,6 +1,6 @@
 <template>
     <a-layout-header class="header">
-        <div class="logo" />
+        <div class="logo">小王知识库</div>
         <a-popconfirm
                 title="确认退出登录?"
                 ok-text="是"
@@ -137,6 +137,15 @@
 </script>
 
 <style>
+    .logo {
+        width: 120px;
+        height: 31px;
+        /*background: rgba(255, 255, 255, 0.2);*/
+        /*margin: 16px 28px 16px 0;*/
+        float: left;
+        color: white;
+        font-size: 18px;
+    }
     .login-menu {
         float: right;
         color: white;
